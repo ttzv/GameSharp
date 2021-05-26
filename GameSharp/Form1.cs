9a0944b382be98c1c02ba11a5767f6a5b15ddb7a
Dominik B <d-o-m-i-n@tlen.pl>
2021-05-26 13:46:12 +0200

    timer i automatycznu ruch

--- GameSharp/Form1.cs
+++ GameSharp/Form1.cs
@@ -16,37 +16,101 @@ namespace GameSharp
         {
             InitializeComponent();
         }
+
+        string direction = "right";
         Random R = new Random();
         point[,] punkty = new point[20, 20];
         snake wezyk = new snake();
+
+
+        public System.Timers.Timer timer = new System.Timers.Timer();
+       
+        public void move(object source, System.Timers.ElapsedEventArgs e)
+        {
+            punkty[wezyk.snakesegments[0].y, wezyk.snakesegments[0].x].punkt.BackColor = Color.FromArgb(92, 38, 0);
+
+            if (direction == "right")
+            {
+                if (wezyk.snakesegments[0].x == 19)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Koniec gry");
+                }
+                else
+                {
+                    wezyk.snakesegments[0].x = wezyk.snakesegments[0].x + 1;
+                }
+                
+            }
+            if (direction == "left")
+            {
+                if (wezyk.snakesegments[0].x == 0)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Koniec gry");
+                }
+                else
+                {
+                    wezyk.snakesegments[0].x = wezyk.snakesegments[0].x - 1;
+                }
+            }
+            if (direction == "down")
+            {
+                if (wezyk.snakesegments[0].y == 19)
+                {
+                    timer.Stop();
+                    MessageBox.Show("Koniec gry");
+                }
+                else
+                {
+                    wezyk.snakesegments[0].y = wezyk.snakesegments[0].y + 1;
+                }
+            }
+            if (direction == "up")
+
+            {
+                if (wezyk.snakesegments[0].y == 0)
+                {
+                   
+                    timer.Stop();
+                    MessageBox.Show("Koniec gry");
+                }
+                else
+                {
+                    wezyk.snakesegments[0].y = wezyk.snakesegments[0].y - 1;
+                }
+            }
+            updateGame();
+        }
         public void Generategame()
         {
-            int row = 0;
-            int column = 0;
+           
             for (int i = 0; i < 20; i++)
             {
-                column = 0;
+               
                 for (int j = 0; j < 20; j++)
                 {
                     punkty[i, j] = new point();
                     punkty[i, j].punkt = new Panel();
 
                     // Panel panel1 = new Panel();
-                    punkty[i, j].punkt.Name = "panel" + column + "_" + row;
+                    punkty[i, j].punkt.Name = "panel" + j + "_" + i;
                     punkty[i, j].punkt.BackColor = Color.FromArgb(92, 38, 0);
                     punkty[i, j].punkt.Location = new Point(j * 20, i * 20);
                     punkty[i, j].punkt.Size = new Size(20, 20);
                     punkty[i, j].punkt.Padding = new Padding(0);
                     punkty[i, j].punkt.Margin = new Padding(0);
                     flowLayoutPanel1.Controls.Add(punkty[i, j].punkt);
-                    column++;
+                   
                 }
-                row++;
+            
             }
+         
         }
+
         public void updateGame()
         {
-            punkty[wezyk.snakesegments[0].x, wezyk.snakesegments[0].y].punkt.BackColor = Color.FromArgb(169, 173, 33);
+            punkty[wezyk.snakesegments[0].y, wezyk.snakesegments[0].x].punkt.BackColor = Color.FromArgb(169, 173, 33);
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -63,6 +127,10 @@ namespace GameSharp
         private void button2_Click(object sender, EventArgs e)
         {
             //punkty[0, 0].punkt.BackColor = Color.FromArgb(255, 255, 255);
+            timer.Interval = 700;            
+            timer.Elapsed += move;          
+            timer.AutoReset = true;
+            timer.Enabled = true;
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -70,36 +138,37 @@ namespace GameSharp
             punkty[wezyk.snakesegments[0].x, wezyk.snakesegments[0].y].punkt.BackColor = Color.FromArgb(92, 38, 0);
             if (e.KeyCode == Keys.D)
             {
-                if (wezyk.snakesegments[0].y <19)
+
+                if (wezyk.snakesegments[0].y < 19 && direction != "left")
                 {
-                    wezyk.snakesegments[0].y = wezyk.snakesegments[0].y + 1;
+                    direction = "right";
                 }
                 
-                updateGame();
+               // updateGame();
             }
-            if (e.KeyCode == Keys.S)
+            if (e.KeyCode == Keys.S && direction != "up")
             {
                 if (wezyk.snakesegments[0].x < 19)
                 {
-                    wezyk.snakesegments[0].x = wezyk.snakesegments[0].x + 1;
+                    direction = "down";
                 }
-                updateGame();
+                //updateGame();
             }
-            if (e.KeyCode == Keys.A)
+            if (e.KeyCode == Keys.A && direction !="right")
             {
                 if (wezyk.snakesegments[0].y > 0)
                 {
-                    wezyk.snakesegments[0].y = wezyk.snakesegments[0].y - 1;
+                    direction = "left";
                 }
-                updateGame();
+                //updateGame();
             }
-            if (e.KeyCode == Keys.W)
+            if (e.KeyCode == Keys.W && direction != "down")
             {
                 if (wezyk.snakesegments[0].x > 0)
                 {
-                    wezyk.snakesegments[0].x = wezyk.snakesegments[0].x - 1;
+                    direction = "up";
                 }
-                    updateGame();
+                   // updateGame();
             }
         }
     }

1a668305f424bd2f46921ec99046c23d5b2ced74
Dominik B <d-o-m-i-n@tlen.pl>
2021-05-27 11:40:22 +0200

    dodanie pożywienia

---
+++ GameSharp/SnakeFood.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSharp
+{
+    class SnakeFood
+    {
+        public int x;
+        public int y;
+        public SnakeFood(int col, int row)
+        {
+            this.x = col;
+            this.y = row;
+        }
+    }
+}
